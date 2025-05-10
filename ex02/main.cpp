fe58cea47fe2a4210d965296f1ebbbe7dcd5aa05
jeportie <jeportie@student.42.fr>
2025-05-10 20:54:21 +0000

    push pause

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,24 +6,75 @@
 /*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 12:50:20 by jeportie          #+#    #+#             */
-/*   Updated: 2025/05/06 21:59:21 by jeportie         ###   ########.fr       */
+/*   Updated: 2025/05/10 20:06:16 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "src/class/Base.hpp"
+#include "src/class/A.hpp"
+#include "src/class/B.hpp"
+#include "src/class/C.hpp"
 #include <iostream>
-#include "src/class/Serializer.hpp"
-#include "include/Data.hpp"
+#include <cstdlib>
+#include <ctime>
 
-int main() {
-    Data data = {1, "Test", 3.14f};
+Base* generate(void)
+{
+    int r = double(rand()) / (RAND_MAX + 1.0) * (31);
 
-    uintptr_t raw = Serializer::serialize(&data);
-    Data* deserializedData = Serializer::deserialize(raw);
+    if (r < 11)
+    {
+        A* a = new A;
+        return (static_cast<Base*>(a));
+    }
+    if (r > 10 && r < 21)
+    {
+        B* b = new B;
+        return (static_cast<Base*>(b));
+    }
+    if (r > 20 && r < 31)
+    {
+        C* c = new C;
+        return (static_cast<Base*>(c));
+    }
+    std::cout << "ERROR!" << std::endl;
+    return (NULL);
+}
 
-    if (deserializedData == &data) {
-        std::cout << "Serialization and deserialization successful!" << std::endl;
-    } else {
-        std::cout << "Serialization and deserialization failed!" << std::endl;
+void identify(Base* p)
+{
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+}
+
+void identify(Base& p)
+{
+    if (dynamic_cast<A*>(&p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(&p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(&p))
+        std::cout << "C" << std::endl;
+}
+
+int main()
+{
+    std::srand(std::time(NULL));
+
+    for (int i = 0; i < 10; ++i)
+    {
+        Base* obj = generate();
+        std::cout << "Test " << i + 1 << ": " << std::endl;
+        std::cout << "Id by pointer" << std::endl;
+        identify(obj);
+        std::cout << "Id by ref" << std::endl;
+        identify(*obj);
+        delete obj;
+        std::cout << "------" << std::endl;
     }
 
     return 0;
